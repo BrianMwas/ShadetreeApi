c2cd9a36232acc7ae137be1548ed0d8507865d58
BrianMwas <brnmwas@gmail.com>
2020-03-07 08:53:27 -0800

    second commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,33 +1,34 @@
-// module.exports = {
-//   apps : [{
-//     name: 'API',
-//     script: 'app.js',
 
-//     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-//     args: 'one two',
-//     instances: "3",
-//     autorestart: true,
-//     watch: true,
-//     max_memory_restart: '500M',
-//     exec_mode: "cluster",
-//     //prefix logs with time
-//     time: true,
-//     ignore_watch: ["[\/\\]\./", "node_modules"],
-//     log_format: 'YYYY-MM-DD HH:mm Z',
-//     // combine logs
-//     merge_logs: true,
-//     // time in milliseconds required to force a reload
-//     listen_timeout: 8000,
-//     max_restarts: 10,
-//     // list of commands which will be executed after a pull/upgrade operation.
-//     post_update : ["npm install", "echo launching the app"],
-//     env: {
-//       NODE_ENV: 'development'
-//     },
-//     env_production: {
-//       NODE_ENV: 'production'
-//     }
-//   }],
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: './app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: "3",
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '500M',
+    exec_mode: "cluster",
+    //prefix logs with time
+    time: true,
+    ignore_watch: ["[\/\\]\./", "node_modules"],
+    log_format: 'YYYY-MM-DD HH:mm Z',
+    // combine logs
+    merge_logs: true,
+    // time in milliseconds required to force a reload
+    listen_timeout: 8000,
+    max_restarts: 10,
+    // list of commands which will be executed after a pull/upgrade operation.
+    post_update : ["npm install", "echo launching the app"],
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
 
 //   "deploy" : {
 //     "production" : {
@@ -48,15 +49,3 @@
 //     }
 //   }
 // };
-
-apps : [{
-  name: "app",
-  script: "./app.js",
-  instances: "3",
-  env: {
-    NODE_ENV: "development",
-  },
-  env_production: {
-    NODE_ENV: "production",
-  }
-}]
